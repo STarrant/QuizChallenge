0fb4743ac3fe590769793b02c8dedaebcdd03cae
STarrant <stephenvtarrant@gmail.com>
2021-04-05 10:54:36 +0000

    Mid work save - working on fetch questions object

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,40 @@
 console.log('Script Started'); //To_be_deleted.
 
+let player1Questions = [];
+
+// Function to fetch a number of questions of particular category and difficulty from the open tdb (trivia database)
+function fetchQuestions(numQuestions, category, difficulty) {
+    fetch(`https://opentdb.com/api.php?amount=${numQuestions}&category=${category}&difficulty=${difficulty}&type=multiple`)
+    .then(result => result.json())
+    .then(data => {
+        console.log(data).results;
+        data.results.map( data => {
+            const formattedQuestion = {
+                question: data.questions
+            }
+
+        })
+    })
+
+
+}
+
+// Function to generate an object from returned JSON of opentdb.
+function questionsToObject(data) {
+    const questionSet = data.map(item => {
+        let answerArray = [...item.incorrect_answers, item.correct_answer]
+        answerArray.sort(() => Math.random() - 0.5);   // code courtesy of javascript.info/task/shuffle
+        return {
+            question: item.question,
+            correctAnswer: item.correct_answer,
+            answers: answerArray
+        }
+    })
+    console.log(questionSet);   // To_be_deleted.
+}
+
+/*
+
 // Function to fetch a number of questions of particular category and difficulty from the open tdb (trivia database)
 function fetchQuestions(numQuestions, category, difficulty) {
     fetch(`https://opentdb.com/api.php?amount=${numQuestions}&category=${category}&difficulty=${difficulty}`)
@@ -21,6 +56,18 @@ function questionsToObject(data) {
     console.log(questionSet);   // To_be_deleted.
 }
 
+fetchQuestions(5,10,"hard");
+player1Questions = questionsToObject(data);
+console.log(player1Questions);
+
+
+//console.log(player1Questions[0].correctAnswer);
+//console.log(player1Questions[0].answers[0]);
+//console.log(player1Questions[0].answers[1]);
+//console.log(player1Questions[0].answers[2]);
+//console.log(player1Questions[0].answers[3]);
+
+*/
 document.getElementById('qc_btnAnswerD').addEventListener('click', function(){
     fetchQuestions(10,10,'easy');
 });
@@ -39,7 +86,6 @@ function printQuestion(questionText, answerAText, answerBText, answerCText, answ
     answerC.textContent = answerCText;
     answerD = document.getElementById('qc_txtAnswerD');
     answerD.textContent = answerDText;
-    console.log('Completed printAnswers()'); //To_be_deleted.
 }
 //  Test script for printQuestion.
 //document.getElementById('qc_btnAnswerA').addEventListener('click', function(){
