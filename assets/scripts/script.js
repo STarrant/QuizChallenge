65430187dc37933aa5335762025dbc85ed89f5b2
STarrant <stephenvtarrant@gmail.com>
2021-04-05 16:51:15 +0000

    mid work save - still working on fetch function...

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,95 +1,19 @@
-console.log('Script Started'); //To_be_deleted.
-
-questions = [];
-
-fetch(
-    'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple'
-)
-    .then((res) => {
-        return res.json();
-    })
-    .then((loadedQuestions) => {
-        questions = loadedQuestions.results.map((loadedQuestion) => {
-            const formattedQuestion = {
-                question: loadedQuestion.question,
-            };
-
-            const answerChoices = [...loadedQuestion.incorrect_answers];
-            formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
-            answerChoices.splice(
-                formattedQuestion.answer - 1,
-                0,
-                loadedQuestion.correct_answer
-            );
-
-            answerChoices.forEach((choice, index) => {
-                formattedQuestion['choice' + (index + 1)] = choice;
-            });
-            console.log(formattedQuestion);
-            return formattedQuestion;
-        });
-        //startGame();
-    })
-    .catch((err) => {
-        console.error(err);
-    });
-
-console.log(questions);
-
-/*    
-//CONSTANTS
-const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
-
-startGame = () => {
-    questionCounter = 0;
-    score = 0;
-    availableQuesions = [...questions];
-    getNewQuestion();
-};
-*/
-
-
-
-
-
-
-
-/*
-
-// Function to fetch a number of questions of particular category and difficulty from the open tdb (trivia database)
-function fetchQuestions(numQuestions, category, difficulty) {
-    fetch(`https://opentdb.com/api.php?amount=${numQuestions}&category=${category}&difficulty=${difficulty}`)
-    .then(result => result.json())
-    .then(jsonQuestions => questionsToObject(jsonQuestions.results))
+function getData(amount, category){fetch(`https://opentdb.com/api.php?amount=${amount}&category=${category}`)
+.then(res => res.json())
+.then(data => whatever(data.results))
+          }
+function whatever(data) {
+  const test = data.map(item => {
+    return {
+      question: item.question,
+      correct_answer: item.correct_answer,
+      answers: [...item.incorrect_answers, item.correct_answer]
+    }
+  })
+  console.log(test)
 }
 
-// Function to generate an object from returned JSON of opentdb.
-function questionsToObject(data) {
-    const questionSet = data.map(item => {
-        let answerArray = [...item.incorrect_answers, item.correct_answer]
-        answerArray.sort(() => Math.random() - 0.5);   // code courtesy of javascript.info/task/shuffle
-        return {
-            question: item.question,
-            correctAnswer: item.correct_answer,
-            answers: answerArray
-        }
-    })
-    console.log(questionSet);   // To_be_deleted.
-}
-
-
-fetchQuestions(5,10,"hard");
-console.log(playerQuestionSet);
-
-
-//console.log(player1Questions[0].correctAnswer);
-//console.log(player1Questions[0].answers[0]);
-//console.log(player1Questions[0].answers[1]);
-//console.log(player1Questions[0].answers[2]);
-//console.log(player1Questions[0].answers[3]);
-
-
+getData(10, 10);
 
 // Function to write new question and four answers to the HTML.
 // Function contains five arguments for the text values of the question and four possible answers.
@@ -110,6 +34,6 @@ function printQuestion(questionText, answerAText, answerBText, answerCText, answ
 //document.getElementById('qc_btnAnswerA').addEventListener('click', function(){
 //    printQuestion('test text for question', 'Hello answer a', 'Hello answer b', 'Hello answer c', 'Hello answer d');
 //});
-*/
+
 console.log('Script Ended'); //To_be_deleted.
 //function printQuestion(questionText, answerAText, answerBText, answerCText, answerDText) {
