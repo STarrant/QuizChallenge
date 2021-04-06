b5ea9b3896f90d2113020282d01f2c1c9ed9ebce
STarrant <stephenvtarrant@gmail.com>
2021-04-06 11:33:11 +0000

    Feat:  Category list to array function created
    - manually imported the JSON object from
     https://opentdb.com/api_category.php
    - categoryArray created and objects mapped across.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,65 @@
+// Categories list from https://opentdb.com/api_category.php
+
+let opentdbCategoriesObject = {
+    "trivia_categories":[
+        {"id":9,"name":"General Knowledge"},
+        {"id":10,"name":"Entertainment: Books"},
+        {"id":11,"name":"Entertainment: Film"},
+        {"id":12,"name":"Entertainment: Music"},
+        {"id":13,"name":"Entertainment: Musicals & Theatres"},
+        {"id":14,"name":"Entertainment: Television"},
+        {"id":15,"name":"Entertainment: Video Games"},
+        {"id":16,"name":"Entertainment: Board Games"},
+        {"id":17,"name":"Science & Nature"},
+        {"id":18,"name":"Science: Computers"},
+        {"id":19,"name":"Science: Mathematics"},
+        {"id":20,"name":"Mythology"},
+        {"id":21,"name":"Sports"},
+        {"id":22,"name":"Geography"},
+        {"id":23,"name":"History"},
+        {"id":24,"name":"Politics"},
+        {"id":25,"name":"Art"},
+        {"id":26,"name":"Celebrities"},
+        {"id":27,"name":"Animals"},
+        {"id":28,"name":"Vehicles"},
+        {"id":29,"name":"Entertainment: Comics"},
+        {"id":30,"name":"Science: Gadgets"},
+        {"id":31,"name":"Entertainment: Japanese Anime & Manga"},
+        {"id":32,"name":"Entertainment: Cartoon & Animations"}]
+    };
+let opentdbCategoriesArray = opentdbCategoriesObject.trivia_categories;
+
+var categoryArray = opentdbCategoriesArray.map( function(category) {
+    var item = {
+        "id": category.id,
+        "name": category.name
+        }
+    return item;
+});
+
+console.log(categoryArray[3]);
+console.log(categoryArray[3].id)
+console.log(categoryArray[3].name)
+
+// Example Script to be deleted. --------------------------------------------------------------------------------------------
+var orders = [ { "name" : "chain", "description" : "necklace chain", "status": "shipped"} , {"name": "pen", "description" : "ball pen", "status": "shipped"}, {"name": "book", "description" : "travel diary", "status": "delivered"},{"name": "brush", "description" : "paint brush", "status": "delivered"}];
+console.log(orders); 
+var orderInfo = orders.map( function(order) {
+ if( order.status === "delivered"){
+     var info = { "orderName": order.name,
+                  "orderDesc": order.description
+                 }
+     return info;
+ }
+});
+console.log(orderInfo);
+// End of Example Script to be deleted. --------------------------------------------------------------------------------------------
+
+
+/*
+
+
+
 function getData(amount, category){fetch(`https://opentdb.com/api.php?amount=${amount}&category=${category}`)
 .then(res => res.json())
 .then(data => whatever(data.results))
@@ -34,6 +96,5 @@ function printQuestion(questionText, answerAText, answerBText, answerCText, answ
 //document.getElementById('qc_btnAnswerA').addEventListener('click', function(){
 //    printQuestion('test text for question', 'Hello answer a', 'Hello answer b', 'Hello answer c', 'Hello answer d');
 //});
-
+*/
 console.log('Script Ended'); //To_be_deleted.
-//function printQuestion(questionText, answerAText, answerBText, answerCText, answerDText) {
