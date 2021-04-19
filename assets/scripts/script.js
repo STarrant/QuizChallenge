98d0d394cc989a588f15e7adfa25f395c7e73d61
STarrant <stephenvtarrant@gmail.com>
2021-04-19 22:38:59 +0000

    Fix:   Issue with link to audio files continued...

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -291,11 +291,11 @@ function highContrastMode() {
 // Credit:  Code here modified from answer by Uri (https://stackoverflow.com/users/378594/uri) in question 9419263 (https://stackoverflow.com/questions/9419263/how-to-play-audio)
 
 function soundCorrect() {
-    let audio = new Audio('../QuizChallenge/assets/audio/335908__littlerainyseasons__correct.mp3');
+    let audio = new Audio('../audio/335908__littlerainyseasons__correct.mp3');
     audio.play();
 }
 
 function soundIncorrect() {
-    let audio = new Audio('../QuizChallenge/assets/audio/196106_aiwha_ding.mp3');
+    let audio = new Audio('../audio/196106_aiwha_ding.mp3');
     audio.play();
 }
